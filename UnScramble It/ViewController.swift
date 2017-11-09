a72d9da97119da7f44ff3f4ae2938ea6a0e20ecd
Sandeep Joshi <aw.snjoshi@hotmail.com>
2017-11-09 01:53:31 -0600

    Initial App

---
+++ UnScramble It/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UnScramble It
+//
+//  Created by Sandeep Joshi on 11/9/17.
+//  Copyright © 2017 Sandeep Joshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
